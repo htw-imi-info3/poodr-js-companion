49b5f295829270d68640547de438e886826a3560
Byron Varberg <byron@byronvarberg.com>
2019-02-03 19:58:16 -0600

    Add example mirroring 3_20.rb
    Demonstrates dependency isolation by making the creation of a Wheel
    explicit in its own method within Gear -- only creating a Wheel object
    when invoked.

---
+++ 3_20.js
@@ -0,0 +1,43 @@
+class Gear {
+  constructor(chainring, cog, rim, tire) {
+    this._chainring = chainring;
+    this._cog = cog;
+    this._rim = rim;
+    this._tire = tire;
+  }
+
+  get chainring() { return this._chainring; }
+  get cog() { return this._cog; }
+  get rim() { return this._rim; }
+  get tire() { return this._tire; }
+
+  gear_inches() {
+    return this.ratio() * this.wheel.diameter();
+  }
+
+  get wheel() {
+    return new Wheel(this.rim, this.tire);
+  }
+
+  ratio() {
+    return this.chainring / this.cog;
+  }
+}
+
+class Wheel {
+  constructor(rim, tire) {
+    this._rim = rim;
+    this._tire = tire;
+  }
+
+  get rim() { return this._rim; }
+  get tire() { return this._tire; }
+
+  diameter() {
+    return this.rim + (this.tire * 2);
+  }
+}
+
+console.log(new Gear(52, 11, 26, 1.5).gear_inches());
+// => 137.0909090909091
+
