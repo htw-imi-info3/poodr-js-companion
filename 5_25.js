85ed7756f110b96fbda1326f226f492e7337ade8
Byron Varberg <byron@byronvarberg.com>
2019-02-19 00:31:47 -0600

    Chicken out of recreating 5_25.rb
    It's best to just go read the section in the book and look at the Ruby
    example with as much context as possible.

---
+++ 5_25.js
@@ -0,0 +1,17 @@
+/*
+ * NOTE: This example is a bit dense to translate because it is exploring code
+ * taken directly from a piece of Ruby on Rails. I've chosen not to try
+ * recreating it here.
+ *
+ * The example aims to show that sometimes checking for an object's
+ * class/type or the messages it responds to can be a useful technique, and that
+ * it does not necessitate replacement with a duck type.
+ *
+ * The code specifically demonstrates checking for stable, core language
+ * classes, where using a duck type would require changing these classes:
+ * possible, but risky.
+ *
+ * Best to just go look at the actual Ruby example -- preferably with the full 
+ * context of the book.
+ */
+
