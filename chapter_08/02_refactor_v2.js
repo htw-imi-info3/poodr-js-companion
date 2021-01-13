fd94a8737618c85970b4028c37c54e582bfbe5d3
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-01-13 12:06:41 +0100

    chapter 08 js

--- chapter_08/02_refactor_v2.js
+++ chapter_08/02_refactor_v2.js
@@ -13,101 +13,34 @@ class Bicycle {
     }
 }
 
-class Parts {
+class Parts extends Array{
+    spares(){
+        return this.filter(p => p.needs_spare)
+    }
+}
+
+class Part {
+
     constructor(opts = {}) {
-        this._chain = opts.chain || this.default_chain();
-        this._tire_size = opts.tire_size || this.default_tire_size();
-        this.post_initialize(opts)
-    }
-    get chain() { return this._chain; }
-    get tire_size() { return this._tire_size; }
 
-    spares() {
-        // see https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-        return Object.assign({}, {
-            tire_size: this.tire_size,
-            chain: this.chain,
-        }, this.local_spares());
+        this._name = opts.name
+        this._description = opts.description
+        this._needs_spare = opts.needs_spare || true
     }
 
-    default_tire_size() {
-        throw new Error('Not implemented');
+    get name() { return this._name; }
+    get description() { return this._description; }
+    get needs_spare() { return this._needs_spare; }
+
+    to_s(){
+        return `${this._name}: ${this._description} (${this._needs_spare ? "needed" : "not needed" })`
     }
 
-    post_initialize() {}
-
-    local_spares() {
-        return {};
-    }
-
-    default_chain() {
-        return '11-speed';
-    }
-
-}
-
-class RoadBikeParts extends Parts {
-    post_initialize(opts) {
-        this._tape_color = opts.tape_color;
-    }
-
-    get tape_color() { return this._tape_color; }
-
-    local_spares() {
-        return {
-            tape_color: this.tape_color,
-        };
-    }
-
-    default_tire_size() {
-        return '23';
-    }
-}
-
-class MountainBikeParts extends Parts {
-    post_initialize(opts) {
-        this._front_shock = opts.front_shock;
-        this._rear_shock = opts.rear_shock;
-    }
-
-    get front_shock() { return this._front_shock; }
-    get rear_shock() { return this._rear_shock; }
-
-    local_spares() {
-        return {
-            front_shock: this.front_shock,
-        };
-    }
-
-    default_tire_size() {
-        return '2.1';
-    }
-}
-
-class RecumbentBikeParts extends Parts {
-    post_initialize(opts) {
-        this._flag = opts.flag;
-    }
-
-    get flag() { return this._flag; }
-
-    local_spares() {
-        return {
-            flag: this.flag,
-        };
-    }
-    default_chain() {
-        return '10-speed';
-    }
-
-    default_tire_size() {
-        return '28';
-    }
 }
 
 module.exports = {
     Bicycle: Bicycle,
-    RoadBikeParts: RoadBikeParts,
-    MountainBikeParts: MountainBikeParts,
-    RecumbentBikeParts: RecumbentBikeParts
+    Parts: Parts,
+    Part: Part
 }
+
--- chapter_08/__tests__/02_refactor_v2.test.js
+++ chapter_08/__tests__/02_refactor_v2.test.js
@@ -1,27 +1,56 @@
-const {Bicycle, RoadBikeParts, MountainBikeParts, RecumbentBikeParts} = require("../02_refactor_v2")
+const {Bicycle, Parts, Part} = require("../02_refactor_v2")
+
 
 describe('all Bikes v2', () => {
+
+    const chain = new Part({name: 'chain', description: '10-speed'})
+
+    const road_tire = new Part({name: 'tire_size', description: '23'})
+
+    const tape = new Part({name: 'tape_color', description: 'red'})
+
+    const mountain_tire = new Part({name: 'tire_size', description: '2.1'})
+
+    const rear_shock = new Part({name: 'rear_shock', description: 'Fox'})
+
+    const front_shock = new Part({
+        name: 'front_shock',
+        description: 'Manitou',
+        needs_spare: false})
+
+    const flag = new Part({name: flag, description: "Flag to be seen", _needs_spare: true})
+
+    it('has a default for needs_spare', () => {
+        expect(chain._needs_spare).toBe(true)
+    })
+
     test('RoadBike', () => {
-        const parts = new RoadBikeParts({
-            tape_color: 'red'})
-        const road_bike = new Bicycle({
-            size: 'M',
-            parts: parts
-        });
+        const road_bike =
+            new Bicycle ({
+                size:  'M',
+                parts: Parts (chain,
+                    road_tire,
+                    tape)});
+
         expect(road_bike.parts.tire_size).toBe("23")
         expect(road_bike.parts.chain).toBe('11-speed')
+        expect(road_bike.spares().length).toEqual(3)
+        expect(road_bike.parts.length).toEqual(4)
         expect(road_bike.spares()).toEqual(
             {tire_size: '23', chain: '11-speed', tape_color: 'red'})
     })
 
     test('MountainBike', () => {
-        const mountain_bike = new Bicycle({
-            size: 'S',
-            parts: new MountainBikeParts({
-            front_shock: 'Manitou',
-            rear_shock: 'Fox'
-            })
-        });
+        const mountain_bike =
+            new Bicycle ({
+                size:  'L',
+                parts: new Parts (chain,
+                    mountain_tire,
+                    front_shock,
+                    rear_shock)})
+
+        expect(mountain_bike.spares().length).toEqual(3)
+        expect(mountain_bike.parts.length).toEqual(3)
 
         expect(mountain_bike.parts.tire_size).toEqual("2.1")
         expect(mountain_bike.parts.chain).toEqual("11-speed")
@@ -31,9 +60,7 @@ describe('all Bikes v2', () => {
     test('RecumbentBike', () => {
         const bent = new Bicycle({
             size: 'M',
-            parts: new RecumbentBikeParts({
-            flag: 'tall and orange'
-            })
+            parts: new Parts(flag)
         });
         expect(bent.parts.tire_size).toEqual("28")
         expect(bent.parts.chain).toEqual("10-speed")
